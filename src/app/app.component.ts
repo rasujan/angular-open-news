1076b4c3b98b0f029c5344e305b1cb12b6b370e2
rasujan <sujan.maharjan555@gmail.com>
2022-12-29 12:40:39 +0545

    Initiated Side Nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'theGlobalTimes';
+
+  @ViewChild(MatSidenav) sideNav!: MatSidenav;
+
+  constructor(private observer: BreakpointObserver) {}
+
+  ngAfterViewInit(): void {
+    this.sideNav.opened = true;
+    this.observer.observe([`(max-width: 768px)`]).subscribe((res) => {
+      if (res.matches) {
+        this.sideNav.mode = 'over';
+        this.sideNav.close();
+      } else {
+        this.sideNav.mode = 'side';
+        this.sideNav.open();
+      }
+    });
+  }
 }
