38f04f012d8b144f1e9411388c4b3501487527d6
rasujan <sujan.maharjan555@gmail.com>
2022-12-30 16:56:27 +0545

    Added key in evn

--- src/app/news.service.ts
+++ src/app/news.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from './../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class NewsService {
-  api_key = '';
+  api_key = environment.newApiKey || '';
 
   constructor(private http: HttpClient) {}
 
